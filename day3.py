70928cd8905137e7123caa9f18e6807ec962eb16
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2018-12-04 07:26:33 +0200

    Added note to function help about missing part 2

--- day3.py
+++ day3.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 input_day3 = pd.read_csv('day3input.txt', sep=' ', names=['index','atsign','coordinates','area'])
 
+
 def overlap(input):
+    '''Day 3: Count overlapping pixels of a cloth. Part 2 of the day solution is missing yet.'''
     def clrdata(input):
         input['coordinates'] = input['coordinates'].str.replace(':','')
         input['area'] = input['area'].str.replace('x',',')
@@ -20,11 +22,11 @@ def overlap(input):
                             cloth.at[int(clrdata(input)['coordinates'][coordint].split(',')[1])+j,int(clrdata(input)['coordinates'][coordint].split(',')[0])+i] = 'T'
                         else:
                             cloth.at[int(clrdata(input)['coordinates'][coordint].split(',')[1])+j,int(clrdata(input)['coordinates'][coordint].split(',')[0])+i] = 'X'
+
     num = 0
     for i in range(len(cloth)):
         num += cloth[i].str.count('T')
     return sum(num)
-
 
 if __name__ == '__main__':
     print(overlap(input_day3))
